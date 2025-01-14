dbff514f7b9e691ef2b468e53d3dc1f4f7297fd1
Jianhong Yin <jiyin@redhat.com>
2025-01-14 09:14:02 +0800

    bypass-mdm.sh: dynamic get data and root volume and optimize code:
    sometimes we get that the root-volume is not mounted on /, or the
    root-volume's name is even not 'Macintosh HD', in this patch we
    get the root,data volume dynamicly instead hard code 'Macintosh HD'
    and 'Macintosh HD - Data'.
    
    and use variables instead of repeatedly copying system paths
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- bypass-mdm.sh
+++ bypass-mdm.sh
@@ -9,94 +9,95 @@ PUR='\033[1;35m'
 CYAN='\033[1;36m'
 NC='\033[0m'
 
+#rootVolume=$(diskutil info / | awk -F: '/Volume Name:/{print $2}' | sed 's/^ *//')
+#if [ "$rootVolume" = "macOS Base System" ]; then
+#	dataVolume=$(ls -1 /Volumes | grep ' - Data$')
+#fi
+
+dataVolume=$(ls -1 /Volumes | grep ' - Data$')
+rootVolume=${dataVolume% - Data}
+rootVolumeMnt="/Volumes/$rootVolume"
+dataVolumeMnt="/Volumes/$dataVolume"
+sysconfdir="/var/db/ConfigurationProfiles/Settings"
+confdir="$rootVolumeMnt$sysconfdir"
+
 # Display header
-echo -e "${CYAN}Bypass MDM By Assaf Dori (assafdori.com)${NC}"
-echo ""
+echo -e "${CYAN}Bypass MDM By Assaf Dori (assafdori.com)${NC}\n"
 
 # Prompt user for choice
 PS3='Please enter your choice: '
 options=("Bypass MDM from Recovery" "Reboot & Exit")
 select opt in "${options[@]}"; do
-    case $opt in
-        "Bypass MDM from Recovery")
-            # Bypass MDM from Recovery
-            echo -e "${YEL}Bypass MDM from Recovery"
-            if [ -d "/Volumes/Macintosh HD - Data" ]; then
-                diskutil rename "Macintosh HD - Data" "Data"
-            fi
+	case $opt in
+	"Bypass MDM from Recovery")
+		# Bypass MDM from Recovery
+		echo -e "${YEL}Bypass MDM from Recovery"
 
-            # Create Temporary User
-            echo -e "${NC}Create a Temporary User"
-            read -p "Enter Temporary Fullname (Default is 'Apple'): " realName
-            realName="${realName:=Apple}"
-            read -p "Enter Temporary Username (Default is 'Apple'): " username
-            username="${username:=Apple}"
-            read -p "Enter Temporary Password (Default is '1234'): " passw
-            passw="${passw:=1234}"
+		# Create Temporary User
+		echo -e "${NC}Create a Temporary User"
+		read -p "Enter Temporary Fullname (Default is 'Apple'): " realName
+		realName="${realName:=Apple}"
+		read -p "Enter Temporary Username (Default is 'Apple'): " username
+		username="${username:=Apple}"
+		read -p "Enter Temporary Password (Default is '1234'): " passw
+		passw="${passw:=1234}"
 
-            # Create User
-            dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default'
-            echo -e "${GREEN}Creating Temporary User"
-            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
-            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
-            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
-            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
-            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
-            mkdir "/Volumes/Data/Users/$username"
-            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
-            dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
-            dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
+		# Create User
+		dscl_path="${dataVolumeMnt}/private/var/db/dslocal/nodes/Default"
+		user_path=/Local/Default/Users
+		group_path=/Local/Default/Groups
+		echo -e "${GREEN}Creating Temporary User"
+		dscl -f "$dscl_path" localhost -create "$user_path/$username"
+		dscl -f "$dscl_path" localhost -create "$user_path/$username" UserShell "/bin/zsh"
+		dscl -f "$dscl_path" localhost -create "$user_path/$username" RealName "$realName"
+		dscl -f "$dscl_path" localhost -create "$user_path/$username" UniqueID "501"
+		dscl -f "$dscl_path" localhost -create "$user_path/$username" PrimaryGroupID "20"
+		mkdir "${dataVolumeMnt}/Users/$username"
+		dscl -f "$dscl_path" localhost -create "$user_path/$username" NFSHomeDirectory "/Users/$username"
+		dscl -f "$dscl_path" localhost -passwd "$user_path/$username" "$passw"
+		dscl -f "$dscl_path" localhost -append "$group_path/admin" GroupMembership $username
 
-            # Block MDM domains
-            echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
-            echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
-            echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
-            echo -e "${GRN}Successfully blocked MDM & Profile Domains"
+		# Block MDM domains
+		hostsfile="$rootVolumeMnt/etc/hosts"
+		echo -e "0.0.0.0 deviceenrollment.apple.com\n0.0.0.0 mdmenrollment.apple.com\n0.0.0.0 iprofiles.apple.com" >>"$hostsfile"
+		echo -e "${GRN}Successfully blocked MDM & Profile Domains"
 
-            # Remove configuration profiles
-            touch /Volumes/Data/private/var/db/.AppleSetupDone
-            rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-            rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-            touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-            touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+		# Remove configuration profiles
+		touch "${dataVolumeMnt}/private/var/db/.AppleSetupDone"
+		rm -rf "$confdir/.cloudConfigHasActivationRecord" "$confdir/.cloudConfigRecordFound"
+		touch "$confdir/.cloudConfigProfileInstalled" "$confdir/.cloudConfigRecordNotFound"
 
-            echo -e "${GRN}MDM enrollment has been bypassed!${NC}"
-            echo -e "${NC}Exit terminal and reboot your Mac.${NC}"
-            break
-            ;;
-        "Disable Notification (SIP)")
-            # Disable Notification (SIP)
-            echo -e "${RED}Please Insert Your Password To Proceed${NC}"
-            sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-            sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-            sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-            sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
-            break
-            ;;
-        "Disable Notification (Recovery)")
-            # Disable Notification (Recovery)
-            rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-            rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-            touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-            touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
-            break
-            ;;
-        "Check MDM Enrollment")
-            # Check MDM Enrollment
-            echo ""
-            echo -e "${GRN}Check MDM Enrollment. Error is success${NC}"
-            echo ""
-            echo -e "${RED}Please Insert Your Password To Proceed${NC}"
-            echo ""
-            sudo profiles show -type enrollment
-            break
-            ;;
-        "Reboot & Exit")
-            # Reboot & Exit
-            echo "Rebooting..."
-            reboot
-            break
-            ;;
-        *) echo "Invalid option $REPLY" ;;
-    esac
+		echo -e "${GRN}MDM enrollment has been bypassed!${NC}"
+		echo -e "${NC}Exit terminal and reboot your Mac.${NC}"
+		break
+		;;
+	"Disable Notification (SIP)")
+		# Disable Notification (SIP)
+		echo -e "${RED}Please Insert Your Password To Proceed${NC}"
+		sudo rm -rf $sysconfdir/.cloudConfigHasActivationRecord $sysconfdir/.cloudConfigRecordFound
+		sudo touch $sysconfdir/.cloudConfigProfileInstalled $sysconfdir/.cloudConfigRecordNotFound
+		break
+		;;
+	"Disable Notification (Recovery)")
+		# Disable Notification (Recovery)
+		rm -rf "$confdir/.cloudConfigHasActivationRecord" "$confdir/.cloudConfigRecordFound"
+		touch "$confdir/.cloudConfigProfileInstalled" "$confdir/.cloudConfigRecordNotFound"
+		break
+		;;
+	"Check MDM Enrollment")
+		# Check MDM Enrollment
+		echo -e "\n${GRN}Check MDM Enrollment. Error is success${NC}"
+		echo -e "\n${RED}Please Insert Your Password To Proceed${NC}"
+		echo ""
+		sudo profiles show -type enrollment
+		break
+		;;
+	"Reboot & Exit")
+		# Reboot & Exit
+		echo "Rebooting..."
+		reboot
+		break
+		;;
+	*) echo "Invalid option $REPLY" ;;
+	esac
 done
